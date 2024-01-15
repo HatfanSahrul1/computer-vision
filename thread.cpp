00abc47cae307930259d9066483765a04145b9e3
NebulaHans <ronithedoctor46@gmail.com>
2024-01-15 16:29:05 +0700

    mine

--- thread.cpp
+++ thread.cpp
@@ -3,32 +3,56 @@
 
 using namespace std;
 
+int t;
+
+void asd(){
+    cout<<"void"<<endl;
+}
+
+void poin(){
+    // int f=*t;
+    // int d=&f.str();
+    // cout<<d<<endl;
+}
+
 class obj{
     public :
         void dr(int x){
             for(int i=0;i<x;i++){
-                cout<<"asdf"<<endl;
+                t++;
+                //cout<<"asdf"<<endl;
+                //cout<<t<<" ini di fungsi dr"<<endl;
             }
+        }
+
+        static void tr(){
+            cout<<"static"<<endl;
         } 
 };
 class thread_obj {
 public:
     void operator()(int x)
     {
-        for (int i = 0; i < x; i++)
-            cout << "Thread using function"
-                    " object as callable\n";
+        for (int i = 0; i < x; i++){
+            t--;
+            //cout << "Thread using function object as callable\n";
+            //cout<<t<<" ini di calss thread_obj"<<endl;
+        }
+
     }
 };
 
 int main(){
     obj b;
-    thread cb(&obj::dr,&b,3);
-    thread cb2(thread_obj(),3);
+    //t=24;
+    thread cb(&obj::dr,&b,1000000);
+    thread cb2(thread_obj(),1000000);
 
     cb.join();
-    
+
     cb2.join();
 
+    //poin();
+    cout<<t<<endl;
     return 0;
 }
