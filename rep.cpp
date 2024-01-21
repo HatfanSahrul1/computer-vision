f8286ec151bfe2214f00bf195708a2a9169c7c6f
NebulaHans <ronithedoctor46@gmail.com>
2024-01-21 20:49:53 +0700

    rep

--- rep.cpp
+++ rep.cpp
@@ -26,30 +26,29 @@ int main() {
     int h = 0, l = 200, s = 0;
     int H = 180, L = 255, S = 255;
     
-    string path = "/home/hatfan/dataset_garis1/dataset_garis/images/";///home/hatfan/dataset_garis1/dataset_garis/images/train
+    string path = "/home/hatfan/dataset";///home/hatfan/dataset_garis1/dataset_garis/images/train
     stringstream filename, writeName, name, ann;
 
     // p1.join();    
     namedWindow("morphSize",WINDOW_AUTOSIZE);
-    createTrackbar("morphSize","morphSize",&morph_size,15,0);
-    int i =0;
-    while (i <3460) {
-        isEditing=true;
+    // createTrackbar("morphSize","morphSize",&morph_size,15,0);
+    int i=0;
+    while (i <1460) {
+        filename.str("");
+        name.str("");
+        writeName.str("");
+        ann.str("");
         int a;
-        filename << path << "/train/frame_" << i << ".jpg";
-        ann<< path << "/train/frame_" << i << ".xml";
+        filename << path << "/garis/frame_" << i << ".jpg";
+        ann<< path << "/gairs/frame_" << i << ".xml";
         name << "frame_" << i << ".jpg";  
 
         cv::Mat img_gray = cv::imread(filename.str(), 1);
 
         if (img_gray.empty()) {
             cout << name.str() << " not found" << endl;
-            filename.str("");
-            name.str("");
-            writeName.str("");
-            ann.str("");
             i++;
-            continue;
+            // continue;
         }
 
         // FileStorage fs(ann.str(), FileStorage::READ);
@@ -57,36 +56,40 @@ int main() {
         // fs[ann.str()] >> boundingBoxes;
         // fs.release();
 
-        
 
         Mat hls, mask;
         cvtColor(img_gray, hls, COLOR_BGR2HLS);
 
 
-            Mat element = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 2 * morph_size + 1));//, Point(morph_size, morph_size));
+            Mat element = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 2 * morph_size + 1)); //, Point(morph_size, morph_size));
             morphologyEx(hls, hls, MORPH_CLOSE, element, Point(-1, -1), 2);
 
             inRange(hls, Scalar(0, 150, 0), Scalar(180, 255, 255), mask);
-            cv::imshow(name.str(), mask);
+            // cv::imshow(name.str(), mask);
+            writeName << path << "/mask/frame_" << i << ".jpg";
+            imwrite(writeName.str(), mask);
  
 
         // for (const Rect& box : boundingBoxes) {
         //     rectangle(mask, box, Scalar(0, 255, 0), 2);  // Green rectangle
         // }
-        a = waitKey(30);
+        // a = waitKey(30);
 
-        if (a == 32) {
-            // Save the processed image
-            writeName << path << "/mask/frame_" << i << ".jpg";
-            cv::imwrite(writeName.str(), mask);
-             cv::destroyWindow(name.str());
-            i++;
-            //cout << "Image saved: " << writeName.str() << endl;
-        } else if (a == 27) {
-            cout << "kurang pas: " << name.str() << endl;
-            cv::destroyWindow(name.str());
-            i++;
-        }
+        // if (a == 32) {
+        //     // Save the processed image
+        //     writeName << path << "/mask/frame_" << i << ".jpg";
+        //     cv::imwrite(writeName.str(), mask);
+        //      cv::destroyWindow(name.str());
+        //     i++;
+        //     //cout << "Image saved: " << writeName.str() << endl;
+        // } else if (a == 27) {
+        //     cout << "kurang pas: " << name.str() << endl;
+        //     cv::destroyWindow(name.str());
+        //     i++;
+        // }else if (a == 97) {
+        //     cv::destroyWindow(name.str());
+        //     i--;
+        // }
 
         // Clear stringstream variables for the next iteration
         filename.str("");
@@ -94,6 +97,8 @@ int main() {
         writeName.str("");
         ann.str("");
         i++;
+
+        // cv::destroyWindow(name.str());
         
        
     }
