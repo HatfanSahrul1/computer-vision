e1f73d91e28d0a1d537fbbb738d0d4ad7389bb9c
NebulaHans <ronithedoctor46@gmail.com>
2024-01-21 15:05:42 +0700

    roi tahap 1

---
+++ belajarROI.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Point 
+
+int main(){
+    Mat img=imread("/home/hatfan/test2/test.jpg",1);
+    Mat roi;
+
+    Rect rect(img.cols/4,img.rows/4, img.cols/2,img.rows/2);
+
+    rectangle(img,rect,Scalar(255),2,8,0);
+
+    imshow("img",img);
+
+    roi=img(rect);
+
+    imshow("img2",roi);
+
+    waitKey(0);
+    return 0;
+}
--- build/CMakeFiles/main.dir/cmake_clean.cmake
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/main.dir/rep.cpp.o"
+  "CMakeFiles/main.dir/belajarROI.cpp.o"
   "main"
   "main.pdb"
 )
