3890a1eb0e39be6396e908c355ce2a192eed5a5e
NebulaHans <ronithedoctor46@gmail.com>
2024-01-25 20:57:06 +0700

    biar gak mondar mandir

---
+++ build/CMakeFiles/L.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L.dir/inference.cpp.o"
+  "CMakeFiles/L.dir/yolov8Normal.cpp.o"
+  "L"
+  "L.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
