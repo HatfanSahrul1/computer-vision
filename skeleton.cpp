df15fa8a2339377f35d3ddf5ef7728971436988c
HansNebula <hatfansahrul1@gmail.com>
2024-07-26 17:18:24 +0700

    using houghneP and display it to im

--- skeleton.cpp
+++ skeleton.cpp
@@ -16,16 +16,25 @@ int main(int argc, char** argv)
 
     Mat im = imread("/home/hatfan/fokusL/mask/cap_mask_195.jpg");
     Mat skel = skeletonization(im);
-    Mat colored, mask = Mat::zeros(im.size(), CV_8UC3);
+    Mat colored;
     Coloring(skel, colored);
+    Mat skelBinary;
+    threshold(skel, skelBinary, 0, 255, THRESH_BINARY);
 
-    // bitwise_not(im, mask, colored);
+    vector<Vec4i> lines;
+    HoughLinesP(skelBinary, lines, 1, CV_PI / 180, 50, 10, 10);
 
+    // Draw the lines on the original image
+    Mat imWithLines = im.clone();
+    for (size_t i = 0; i < lines.size(); i++) {
+        Vec4i l = lines[i];
+        line(imWithLines, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2, LINE_AA);
+    }
     // namedWindow("Original Image", WINDOW_NORMAL);
     // namedWindow("Skeleton Image", WINDOW_NORMAL);
     imshow("Original Image", im);
     imshow("Skeleton Image", skel);
-    imshow("colored Image", mask);
+    imshow("colored Image", imWithLines);
     waitKey(0);
 
     return 0;
