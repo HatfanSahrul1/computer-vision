1420f442bf6b6b84df92e9bd7d692ba0a88ab03e
HansNebula <hatfansahrul1@gmail.com>
2024-07-26 17:23:42 +0700

    regular houghline

--- skeleton.cpp
+++ skeleton.cpp
@@ -21,14 +21,21 @@ int main(int argc, char** argv)
     Mat skelBinary;
     threshold(skel, skelBinary, 0, 255, THRESH_BINARY);
 
-    vector<Vec4i> lines;
-    HoughLinesP(skelBinary, lines, 1, CV_PI / 180, 50, 10, 10);
+    vector<Vec2f> lines;
+    HoughLines(skelBinary, lines, 1, CV_PI / 180, 100);  // Adjust the threshold as needed
 
     // Draw the lines on the original image
     Mat imWithLines = im.clone();
     for (size_t i = 0; i < lines.size(); i++) {
-        Vec4i l = lines[i];
-        line(imWithLines, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2, LINE_AA);
+        float rho = lines[i][0], theta = lines[i][1];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a * rho, y0 = b * rho;
+        pt1.x = cvRound(x0 + 1000 * (-b));
+        pt1.y = cvRound(y0 + 1000 * (a));
+        pt2.x = cvRound(x0 - 1000 * (-b));
+        pt2.y = cvRound(y0 - 1000 * (a));
+        line(imWithLines, pt1, pt2, Scalar(0, 0, 255), 2, LINE_AA);
     }
     // namedWindow("Original Image", WINDOW_NORMAL);
     // namedWindow("Skeleton Image", WINDOW_NORMAL);
