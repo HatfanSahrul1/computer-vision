5cba3068b4dd8f1964f6b604b4c9fd5a2e7f6ea8
HansNebula <hatfansahrul1@gmail.com>
2024-07-27 11:19:34 +0700

    done with houghline for now

--- skeleton.cpp
+++ skeleton.cpp
@@ -14,23 +14,23 @@ int main(int argc, char** argv)
     //     return -1;
     // }
 
-    Mat im = imread("/home/hatfan/fokusL/mask/cap_mask_195.jpg");
+    Mat im = imread("/home/hatfan/T_2/mask/cap_mask_3019.jpg");
     Mat skel = skeletonization(im);
-    Mat colored;
-    Coloring(skel, colored);
-    Mat skelBinary;
-    threshold(skel, skelBinary, 0, 255, THRESH_BINARY);
-    Mat imWithLines = im.clone();
+    
+    // Mat colored;
+    // Coloring(skel, colored);
+    // Mat skelBinary;
+    // threshold(skel, skelBinary, 0, 255, THRESH_BINARY);
+    // Mat imWithLines = im.clone();
     // Mat imWithLines=Mat::zeros(im.size(), CV_8UC3);
     
     // HoughlineRegular(skelBinary, imWithLines, atoi(argv[1]));
-    HoughlineProb(skelBinary, imWithLines);
-    cout<<argv[1]<<endl;
+    // HoughlineProb(skelBinary, imWithLines, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
     
 
     imshow("Original Image", im);
     imshow("Skeleton Image", skel);
-    imshow("colored Image", imWithLines);
+    // imshow("colored Image", imWithLines);
     waitKey(0);
 
     return 0;
